980c68985364a0920e076337159cf96f9a076abc
peyo <peteryyoon@gmail.com>
2020-04-21 15:28:46 -0700

    bug fixes.

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (id, name)
+VALUES
+  (1, 'folder one'),
+  (2, 'folder dos'),
+  (3, 'folder tres')
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (name, content, folder_id)
+VALUES
+  ('note one', 'Hello World!', 1),
+  ('note two', 'Hello Galaxy!', 2),
+  ('note three', 'Hello Universe!', 3)
