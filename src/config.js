2177f725ee22a591217d28d1d45eb38945693875
peyo <peteryyoon@gmail.com>
2020-04-23 11:52:16 -0700

    updated app.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://peteryoon:pg9177@localhost/noteful",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://peteryoon:pg9177@localhost/noteful",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://peteryoon:pg9177@localhost/noteful-test"
 }
